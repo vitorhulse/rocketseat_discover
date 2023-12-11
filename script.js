4480b2c41adc85e37d7e02305376fca082784ad1
Steckert <vitor.steckert@thomsonreuters.com>
2023-12-10 22:22:54 -0300

    Primeiro commit initial commit

---
+++ script.js
@@ -0,0 +1,16 @@
+function toggleMode() {
+  const html = document.documentElement
+  html.classList.toggle("light")
+
+  // pegar a tag img
+  const img = document.querySelector("#profile img")
+
+  //substituir a imagem
+  if (html.classList.contains("light")) {
+  // se tiver ligh mode, adicionar imagem light
+  img.setAttribute ("src", "./assets/avatar-light.png")
+  } else {
+    // se tiver sem ligh mode, manter a imagem normal
+    img.setAttribute("src", "./assets/avatar.png")
+  }
+}
